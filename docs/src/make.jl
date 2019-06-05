dc86b0df9522b276526e718ed207a80bf3d68cea
Eric Torkia <etorkia@technologypartnerz.com>
2019-06-05 17:54:45 -0400

    updated registration

--- docs/src/make.jl
+++ docs/src/make.jl
@@ -1,6 +1,14 @@
-clearconsole()
+#clearconsole()
 #push!(LOAD_PATH,"../src")
 using Documenter, MCHammer
-makedocs(sitename="MC Hammer Documentation", modules =[MCHammer], doctest = true, root = "Z:\\Program Dev\\Risk & Simulation\\Solution Dev\\Julia\\mc_hammer\\docs", repo = "https://github.com/etorkia/MCHammer.jl")
+makedocs(sitename="MCHammer.jl", modules =[MCHammer], doctest = true, root = "Z:\\Program Dev\\Risk & Simulation\\Solution Dev\\Julia\\mc_hammer\\docs", repo = "github.com/etorkia/MCHammer.jl")
+
+deploydocs(
+    #target="build",
+    repo = "github.com/etorkia/MCHammer.jl.git",
+    #branch = "gh-pages",
+    #devbranch="master",
+    #versions = ["stable" => "v^", "v#.#"]
+)
 
 #Literate.markdown("examples/NPV_testmodel.jl", "docs/src/tutorials"; documenter=true)
