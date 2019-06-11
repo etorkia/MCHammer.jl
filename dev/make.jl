317468f5a813257320f33ca44a96ee6d8197fe5b
zeptodoctor <44736852+zeptodoctor@users.noreply.github.com>
2019-06-11 21:39:54 +0000

    build based on 4776a2d

---
+++ dev/make.jl
@@ -0,0 +1,18 @@
+#clearconsole()
+#push!(LOAD_PATH,"../src")
+using Pkg
+Pkg.add("Documenter")
+Pkg.add("DocumenterTools")
+using Documenter, DocumenterTools, MCHammer
+
+makedocs(sitename="MCHammer.jl", modules =[MCHammer, Documenter, DocumenterTools], doctest = true, repo = "github.com/etorkia/MCHammer.jl")
+
+deploydocs(
+    #target="build",
+    repo = "github.com/etorkia/MCHammer.jl.git",
+    #branch = "gh-pages",
+    #devbranch="master",
+    #versions = ["stable" => "v^", "v#.#"]
+)
+
+#Literate.markdown("examples/NPV_testmodel.jl", "docs/src/tutorials"; documenter=true)
