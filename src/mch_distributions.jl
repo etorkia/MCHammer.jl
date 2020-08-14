8d65d87ee68a1e4efe8d2065ebee7b9c26953535
etorkia <etorkia@technologypartnerz.com>
2020-08-14 13:55:13 -0400

    Risk Events

--- src/mch_distributions.jl
+++ src/mch_distributions.jl
@@ -7,3 +7,14 @@ function mlprobs(Array, Increment)
     ArrayLength = size(Array,1)
     SA = sort(Array,1,rev=false)
     ML_Probs = []
+end
+
+"""
+    RiskEvent(Prob, Distribution, Trials)
+
+Risk Events are defines as conditional distributions that will inflate the 0....
+"""
+function RiskEvent(Prob, Distribution, Trials)
+    C_dist = rand(Bernoulli(Prob),Trials) .* rand(Distribution,Trials)
+    return C_dist
+end
