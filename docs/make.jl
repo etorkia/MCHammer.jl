2349c95de7cec70267ffbad61af5ef167a36fcec
Eric Torkia <etorkia@technologypartnerz.com>
2025-03-28 23:45:55 -0400

    documenter

--- docs/make.jl
+++ docs/make.jl
@@ -1,59 +1,52 @@
-#clearconsole()
-push!(LOAD_PATH,"../src")
-#push!(LOAD_PATH, joinpath(@__DIR__, ".."))
-using Pkg
-using Dates
-using DataFrames
-using Distributions, Statistics, StatsBase
+push!(LOAD_PATH, joinpath(@__DIR__, "..", "src"))
+using Pkg, Dates, DataFrames, Distributions, Statistics, StatsBase
 using Plots, StatsPlots, GraphRecipes
-using Documenter,Test, DocumenterTools,Revise
-using MCHammer, Logging, LoggingExtras, Documenter
+using Documenter, Test, DocumenterTools, Revise
+using MCHammer, Logging, LoggingExtras
 
 makedocs(
-sitename="MCHammer.jl",
-modules =[MCHammer],
-pages = Any[
-    "Home" => "index.md",
-    "User Manual" => Any[
-        "Monte-Carlo Simulation" => "manual/1_functions.md",
-        "Distribution Fitting" => "manual/distribution_fitting.md",
-        "Charting & Analyzing" => "manual/2_charts.md",
-        "Time-Series Functions" => Any[
-            "Random & Probability Methods"     => "manual/3_time_series.md",
-            "Exponential Smoothing" => "manual/ExponentialSmoothing.md",
-            "Learning Curves Models" => "manual/LearningCurves.md"
-            ],
-                
+    sitename = "MCHammer.jl",
+    modules = [MCHammer],
+    pages = Any[
+        "Home" => "index.md",
+        "User Manual" => Any[
+            "Monte-Carlo Simulation" => "manual/1_functions.md",
+            "Distribution Fitting" => "manual/distribution_fitting.md",
+            "Charting & Analyzing" => "manual/2_charts.md",
+            "Time-Series Functions" => Any[
+                "Random & Probability Methods"     => "manual/3_time_series.md",
+                "Exponential Smoothing" => "manual/ExponentialSmoothing.md",
+                "Learning Curves Models" => "manual/LearningCurves.md"
+            ]
+        ],
+        "Tutorials" => Any[
+            "My First Model" => "tutorials/1_first_model.md",
+            "Correlating Inputs" => "tutorials/2_Correlated_Model.md",
+            "Simulated CashFlow Model" => "tutorials/3_NPV_testmodel.md"
+        ],
+        "Import / Export Data" => "manual/4_moving_results.md"
     ],
-    "Tutorials" => Any[
-        "My First Model" => "tutorials/1_first_model.md",
-        "Correlating Inputs" => "tutorials/2_Correlated_Model.md",
-        "Simulated CashFlow Model" => "tutorials/3_NPV_testmodel.md",
-    ],
-    "Import / Export Data" => "manual/4_moving_results.md"
-    ],
-
-format = Documenter.HTML(
-    # Use clean URLs, unless built as a "local" build
-    prettyurls = get(ENV, "CI", nothing) == "true",
-    canonical = "https://etorkia.github.io/MCHammer.jl/",
-    assets = ["assets/favicon.ico"],
-    analytics = "UA-3913053-5",
-),
-
-authors = "Eric Torkia and contributors",
-doctest = true,
-source  = "src",
-build   = "build",
-clean   = true,
-repo = "github.com/etorkia/MCHammer.jl")
+    format = Documenter.HTML(
+        prettyurls = get(ENV, "CI", nothing) == "true",
+        canonical = "https://etorkia.github.io/MCHammer.jl/",
+        assets = ["assets/favicon.ico"],
+        analytics = "UA-3913053-5"
+    ),
+    authors = "Eric Torkia and contributors",
+    doctest = true,
+    source = "src",
+    build = "build",
+    clean = true,
+    repo = "https://github.com/etorkia/MCHammer.jl"
+)
 
 deploydocs(
-    target="build",
-    repo = "github.com/etorkia/MCHammer.jl.git",
+    target = "build",
+    repo = "https://github.com/etorkia/MCHammer.jl.git",
     branch = "gh-pages",
-    devbranch="master",
+    devbranch = "master",
     versions = ["stable" => "v^", "v#.#"]
 )
+
 
 #deploydocs(repo = "github.com/etorkia/MCHammer.jl.git")
